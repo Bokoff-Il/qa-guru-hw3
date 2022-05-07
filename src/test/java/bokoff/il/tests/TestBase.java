fdde151644ae685cc97cc1b3141af70c9d42e983
Ilya Bokov <ibokov@rvision.ru>
2022-05-07 21:06:21 +0300

    Added owner and system properties

--- src/test/java/bokoff/il/tests/TestBase.java
+++ src/test/java/bokoff/il/tests/TestBase.java
@@ -1,9 +1,11 @@
 package bokoff.il.tests;
 
+import bokoff.il.config.CredentialsConfig;
 import bokoff.il.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,11 +13,17 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
   @BeforeAll
   static void setUp(){
+
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-    Configuration.baseUrl = "https://demoqa.com";
-    Configuration.browserSize = "1920x1080";
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    String login = config.login();
+    String password = config.password();
+    String remote = System.getProperty("remote", "selenoid.autotests.cloud/wd/hub");
+
+    Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+    Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+    Configuration.remote = "https://"+login+":"+password+"@"+remote;
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("enableVNC", true);
